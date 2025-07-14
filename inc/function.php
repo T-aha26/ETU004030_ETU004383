262bf137336ef21fb631855e860259a28627088b
Tahiry <tahiryaritratiana@gmail.com>
2025-07-14 12:23:22 +0300

    fonction

--- inc/function.php
+++ inc/function.php
@@ -15,6 +15,33 @@ function insert_membre($membre)
     }
 
 }
+
+function get_all_objects()
+{
+    $sql = "SELECT 
+                o.id_objet,
+                o.nom_objet,
+                o.id_categorie,
+                o.id_membre,
+                e.date_retour
+            FROM ExamS2_objet o
+            LEFT JOIN ExamS2_emprunt e 
+                ON o.id_objet = e.id_objet
+                AND (e.date_retour IS NULL OR e.date_retour > CURRENT_DATE)
+            ORDER BY o.nom_objet ASC";
+    
+    $req = mysqli_query(connex(), $sql);
+    $result = array();
+    
+    while ($row = mysqli_fetch_assoc($req)) {
+        $result[] = $row;
+    }
+    
+    mysqli_free_result($req);
+    return $result;
+}
+
+
 function get_membre_by_id($id) {
     $sql = sprintf("SELECT * FROM ExamS2_membre WHERE idMembre = '%s'", $id);
     $res = mysqli_query(connex(), $sql);
